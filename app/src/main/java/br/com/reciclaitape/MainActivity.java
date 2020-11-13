5f828925ee24e87d9e213534f42f8dcca6b15941
Miqueias Fernando <miqueiasfernando@gmail.com>
2020-11-13 19:36:00 -0300

    implementacao mapa

--- app/src/main/java/br/com/reciclaitape/MainActivity.java
+++ app/src/main/java/br/com/reciclaitape/MainActivity.java
@@ -4,11 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng Itape = new LatLng(-23.590117, -48.049640);
+        mMap.addMarker(new MarkerOptions()
+                .position(Itape)
+                .title("Marker em Itapetininga"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Itape));
     }
 }
