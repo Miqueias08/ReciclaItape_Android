3d76d430e9970a303e1e7936b37b0f2c1280a90f
Miqueias Fernando <miqueiasfernando@gmail.com>
2020-11-18 20:32:38 -0300

    Ação BTN voltar

--- app/src/main/java/br/com/reciclaitape/activitys/CadastroPontoActivity.java
+++ app/src/main/java/br/com/reciclaitape/activitys/CadastroPontoActivity.java
@@ -2,6 +2,7 @@ package br.com.reciclaitape.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import br.com.reciclaitape.R;
@@ -12,5 +13,10 @@ public class CadastroPontoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_ponto);
+    }
+    @Override
+    public void onBackPressed() {
+        Intent setIntent = new Intent(this,MapaActivity.class);
+        startActivity(setIntent);
     }
 }
