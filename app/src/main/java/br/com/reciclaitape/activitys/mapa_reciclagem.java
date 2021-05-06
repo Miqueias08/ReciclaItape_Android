0cdc81922b6e428ac994ba3939c41029c32dc3a8
Miqueias Fernando <miqueiasfernando@gmail.com>
2021-05-06 19:06:24 -0300

    updates

--- app/src/main/java/br/com/reciclaitape/activitys/mapa_reciclagem.java
+++ app/src/main/java/br/com/reciclaitape/activitys/mapa_reciclagem.java
@@ -11,6 +11,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -44,7 +45,14 @@ public class mapa_reciclagem extends AppCompatActivity implements OnMapReadyCall
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         float zoomLevel = 15.0f;
+
         mMap.getUiSettings().setMapToolbarEnabled(false);
+        /*Inicia o mapa na cidade de Itapetininga*/
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-23.589158, -48.049099), zoomLevel), 200, null);
+
+       carrega_pontos();
+    }
+    public void carrega_pontos(){
         Call<List<Markers>> listCall = ApiClient.getMarkersService().getMarkers();
         listCall.enqueue(new Callback<List<Markers>>() {
             @Override
@@ -52,8 +60,7 @@ public class mapa_reciclagem extends AppCompatActivity implements OnMapReadyCall
                 if(response.isSuccessful()){
                     for (Markers pnt:response.body()){
                         try {
-                            mMap.addMarker(new MarkerOptions().position(new LatLng(pnt.getLat(),pnt.getLng())).title(pnt.getName()).snippet(pnt.getAddress()));
-                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(pnt.getLat(),pnt.getLng()), zoomLevel), 200, null);
+                            mMap.addMarker(new MarkerOptions().position(new LatLng(pnt.getLat(),pnt.getLng())).title(pnt.getName()).snippet("Telefone:(15)996544878"+"\nEndereço:"+pnt.getAddress()));
                         }
                         catch (Exception e) {
                             Log.e("ERRO", e.getMessage());
@@ -61,16 +68,15 @@ public class mapa_reciclagem extends AppCompatActivity implements OnMapReadyCall
                     }
                 }
                 else{
-                    Toast.makeText(mapa_reciclagem.this, "Erro", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(mapa_reciclagem.this, "Erro", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<List<Markers>> call, Throwable t) {
-                Toast.makeText(mapa_reciclagem.this, "Erro de Conexão", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(mapa_reciclagem.this, "Erro de Conexão", Toast.LENGTH_SHORT).show();
                 Log.e("ERRO",t.getMessage());
             }
         });
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-23.589184, -48.048853)));
     }
 }
