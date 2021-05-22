4e6e5c054439b515851578c164ddc417b4765fa9
Miqueias08 <miqueiasfernando@gmail.com>
2021-05-22 09:22:10 -0300

    cadastro

--- app/src/main/java/br/com/reciclaitape/fragments/cadastro_usuario_fragment.java
+++ app/src/main/java/br/com/reciclaitape/fragments/cadastro_usuario_fragment.java
@@ -7,38 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import br.com.reciclaitape.R;
+import br.com.reciclaitape.classes.Util;
+import br.com.reciclaitape.classes.Util_Navegacao;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link cadastro_usuario_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class cadastro_usuario_fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button btn_voltar,btn_cadastro;
+    Util util = new Util();
+    Util_Navegacao util_navegacao = new Util_Navegacao();
 
     public cadastro_usuario_fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment cadastro_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static cadastro_usuario_fragment newInstance(String param1, String param2) {
         cadastro_usuario_fragment fragment = new cadastro_usuario_fragment();
         Bundle args = new Bundle();
@@ -60,7 +51,22 @@ public class cadastro_usuario_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.cadastro_usuario_fragment, container, false);
+        View view = inflater.inflate (R.layout.fragment_exibir_tutorial, container, false );
+        carrega_componentes(view);
+        voltar();
+        return view;
     }
+    public void carrega_componentes(View view){
+        btn_voltar = (Button) view.findViewById(R.id.btnVoltar_usuario);
+    }
+    public void voltar(){
+        btn_voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                util_navegacao.fragmentClass = login_fragment.class;
+                util_navegacao.navegacao_fragment(getFragmentManager());
+            }
+        });
+    }
+
 }
